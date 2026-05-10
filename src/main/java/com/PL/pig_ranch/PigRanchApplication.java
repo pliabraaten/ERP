0fe546f39b3fe5c546981ce1a35a4edac7fa51a7
parker <pliabraaten@gmail.com>
2026-05-10 10:28:16 -0700

    initial commit

---
+++ src/main/java/com/PL/pig_ranch/PigRanchApplication.java
@@ -0,0 +1,13 @@
+package com.PL.pig_ranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigRanchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigRanchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PL/pig_ranch/PigRanchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PL.pig_ranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigRanchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
